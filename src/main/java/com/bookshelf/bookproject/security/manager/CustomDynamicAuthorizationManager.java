3db300516c69f8a7cea034b7765751779a95384c
minhyung <minhyung66547@gmail.com>
2024-11-16 20:33:36 +0900

    docs: 주석 추가

--- src/main/java/com/bookshelf/bookproject/security/manager/CustomDynamicAuthorizationManager.java
+++ src/main/java/com/bookshelf/bookproject/security/manager/CustomDynamicAuthorizationManager.java
@@ -28,6 +28,13 @@ public class CustomDynamicAuthorizationManager implements AuthorizationManager<R
         this.authorityManagementRepository = authorityManagementRepository;
     }
 
+    /**
+     * 경로와 권한 매핑 정보를 사용하여 {@code mappings} 리스트를 초기화합니다.
+     * <p> 이 메서드는 애플리케이션 시작 시 {@link PostConstruct} 어노테이션에 의해 자동으로 호출되며,
+     * {@link AuthorityManagementRepository}로부터 권한 매핑 정보를 가져와 동적 인가 매핑을 설정합니다.
+     * 각 경로(path)와 권한(role)을 매핑하여 {@link RequestMatcherEntry} 리스트를 생성하고,
+     * 이를 {@code mappings} 필드에 할당합니다.
+     */
     @PostConstruct
     private void initialize() {
         DynamicAuthorizationService dynamicAuthorizationService = new DynamicAuthorizationService(new PathRoleMapper(authorityManagementRepository));
@@ -39,6 +46,16 @@ public class CustomDynamicAuthorizationManager implements AuthorizationManager<R
                 ).toList();
     }
 
+    /**
+     * 요청 경로와 사용자 권한을 기반으로 인가 여부를 결정
+     * <p> 요청된 경로를 {@code mappings}에 있는 경로들과 비교하여 일치하는 매핑을 찾습니다.
+     * 경로가 일치할 경우, 해당 매핑의 권한과 {@code authentication}의 권한을 비교하여 인가 여부를 결정합니다.
+     * 경로가 일치하지 않거나 모든 매핑 조건이 만족하지 않을 경우, 기본적으로 인가를 허용합니다.
+     *
+     * @param authentication 사용자의 인증 정보를 제공하는 {@link Supplier} 객체
+     * @param request 권한을 확인할 대상 요청을 나타내는 {@link RequestAuthorizationContext} 객체
+     * @return 요청이 허용되면 {@code AuthorizationDecision(true)}, 거부되면 {@code AuthorizationDecision(false)}
+     */
     @Override
     public AuthorizationDecision check(Supplier<Authentication> authentication, RequestAuthorizationContext request) {
         for (RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> mapping : mappings) {
--- src/main/java/com/bookshelf/bookproject/security/mapper/PathRoleMapper.java
+++ src/main/java/com/bookshelf/bookproject/security/mapper/PathRoleMapper.java
@@ -14,6 +14,13 @@ public class PathRoleMapper {
         this.authorityManagementRepository = authorityManagementRepository;
     }
 
+    /**
+     * 모든 {@link AuthorityDto} 객체에서 경로와 권한 정보를 조회하여 Map으로 반환합니다.
+     * <p> {@link AuthorityDto} 리스트에서 각 객체의 경로(path)와 권한(role)을 추출하여,
+     * 경로를 key, 권한을 value로 갖는 {@code Map<String, String>} 형태로 반환합니다.
+     *
+     * @return 각 경로(path)를 key로, 해당 권한(role)을 value로 갖는 {@link Map} 객체
+     */
     public Map<String, String> getPathRoleMappings() {
         List<AuthorityDto> authorityList = authorityManagementRepository.findAuthorityAll();
 
